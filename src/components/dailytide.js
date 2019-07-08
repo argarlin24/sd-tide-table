e5358d1104a386144513638d320cd57793bf1e5c
Adam Garling <33332581+argarlin24@users.noreply.github.com>
2019-07-08 16:15:42 -0600

    change chart line weight and added media query for chart width at mobile

--- src/components/dailytide.js
+++ src/components/dailytide.js
@@ -1,67 +1,70 @@
-import React, { Component } from "react"
-import moment from "moment"
-import styled from "styled-components"
-import LineChart from "./linechart"
-import Table from "./table"
-import Spinner from "./spinner"
+import React, { Component } from "react";
+import moment from "moment";
+import styled from "styled-components";
+import LineChart from "./linechart";
+import Table from "./table";
+import Spinner from "./spinner";
 
 class DailyTide extends Component {
-  state = {
-    tideData: null,
-    formattedData: [],
-  }
-  componentDidMount() {
-    this.query()
-  }
+	state = {
+		tideData: null,
+		formattedData: [],
+	};
+	componentDidMount() {
+		this.query();
+	}
 
-  async query() {
-    const begin = moment().format("YYYYMMDD")
-    try {
-      const res = await fetch(
-        `https://tidesandcurrents.noaa.gov/api/datagetter?product=predictions&begin_date=${begin}&end_date=${begin}&datum=MLLW&station=TWC0405&time_zone=lst_ldt&units=english&interval=hilo&format=json`
-      )
-      const data = await res.json()
-      this.setState({
-        tideData: data,
-        loaded: true,
-      })
-      this.formatData(data)
-    } catch (error) {
-      console.log(error)
-    }
-  }
+	async query() {
+		const begin = moment().format("YYYYMMDD");
+		try {
+			const res = await fetch(
+				`https://tidesandcurrents.noaa.gov/api/datagetter?product=predictions&begin_date=${begin}&end_date=${begin}&datum=MLLW&station=TWC0405&time_zone=lst_ldt&units=english&interval=hilo&format=json`
+			);
+			const data = await res.json();
+			this.setState({
+				tideData: data,
+				loaded: true,
+			});
+			this.formatData(data);
+		} catch (error) {
+			console.log(error);
+		}
+	}
 
-  render() {
-    console.log(this.state.tideData)
-    return (
-      <>
-        <GraphWrapper>
-          {this.state.tideData ? (
-            <LineChart data={this.state.tideData} />
-          ) : (
-            <Spinner />
-          )}
-        </GraphWrapper>
-        <TableWrapper>
-          {this.state.tideData ? (
-            <Table data={this.state.tideData} />
-          ) : (
-            <Spinner />
-          )}
-        </TableWrapper>
-      </>
-    )
-  }
+	render() {
+		console.log(this.state.tideData);
+		return (
+			<>
+				<GraphWrapper>
+					{this.state.tideData ? (
+						<LineChart data={this.state.tideData} />
+					) : (
+						<Spinner />
+					)}
+				</GraphWrapper>
+				<TableWrapper>
+					{this.state.tideData ? (
+						<Table data={this.state.tideData} />
+					) : (
+						<Spinner />
+					)}
+				</TableWrapper>
+			</>
+		);
+	}
 }
 
 const GraphWrapper = styled.div`
-  width: 50%;
-  height: auto;
-  margin: 0 auto;
-`
+	width: 50%;
+	height: auto;
+	margin: 0 auto;
+	@media (max-width: 770px) {
+		width: 75%;
+	}
+`;
 
 const TableWrapper = styled.div`
-  padding-top: 60px;
-`
+	padding-top: 60px;
+`;
 
-export default DailyTide
+export default DailyTide;
--- src/components/linechart.js
+++ src/components/linechart.js
@@ -1,49 +1,50 @@
-import React, { Component } from "react"
-import { Line } from "react-chartjs-2"
-import { defaults } from "react-chartjs-2"
-import moment from "moment"
+import React, { Component } from "react";
+import { Line } from "react-chartjs-2";
+import { defaults } from "react-chartjs-2";
+import moment from "moment";
 
 //sets the font color of the graph
-defaults.global.defaultFontColor = "#dddddd"
+defaults.global.defaultFontColor = "#dddddd";
 
 class LineChart extends Component {
-  render() {
-    //time string value parsed into standard local time
-    const time = this.props.data.predictions.map(tide =>
-      moment(tide.t.slice(11, 16), "HH:mm").format("hh:mm a")
-    )
-    // tide value mapped to array
-    const tideValues = this.props.data.predictions.map(tide => tide.v)
-    //chart config
+	render() {
+		//time string value parsed into standard local time
+		const time = this.props.data.predictions.map((tide) =>
+			moment(tide.t.slice(11, 16), "HH:mm").format("hh:mm a")
+		);
+		// tide value mapped to array
+		const tideValues = this.props.data.predictions.map((tide) => tide.v);
+		//chart config
 
-    const data = {
-      labels: time,
-      datasets: [
-        {
-          label: "Tide",
-          fill: false,
-          lineTension: 0.4,
-          backgroundColor: "rgba(75,192,192,0.4)",
-          borderColor: "rgba(75,192,192,1)",
-          borderCapStyle: "butt",
-          borderDash: [],
-          borderDashOffset: 0.0,
-          borderJoinStyle: "miter",
-          pointBorderColor: "rgba(75,192,192,1)",
-          pointBackgroundColor: "#fff",
-          pointBorderWidth: 1,
-          pointHoverRadius: 5,
-          pointHoverBackgroundColor: "rgba(75,192,192,1)",
-          pointHoverBorderColor: "rgba(220,220,220,1)",
-          pointHoverBorderWidth: 2,
-          pointRadius: 5,
-          pointHitRadius: 10,
-          data: tideValues,
-        },
-      ],
-    }
-    return <Line data={data} />
-  }
+		const data = {
+			labels: time,
+			datasets: [
+				{
+					label: "Tide",
+					fill: false,
+					lineTension: 0.4,
+					backgroundColor: "rgba(75,192,192,0.4)",
+					borderWidth: 2,
+					borderColor: "rgba(75,192,192,1)",
+					borderCapStyle: "butt",
+					borderDash: [],
+					borderDashOffset: 0.0,
+					borderJoinStyle: "miter",
+					pointBorderColor: "rgba(75,192,192,1)",
+					pointBackgroundColor: "#fff",
+					pointBorderWidth: 1,
+					pointHoverRadius: 5,
+					pointHoverBackgroundColor: "rgba(75,192,192,1)",
+					pointHoverBorderColor: "rgba(220,220,220,1)",
+					pointHoverBorderWidth: 2,
+					pointRadius: 3,
+					pointHitRadius: 10,
+					data: tideValues,
+				},
+			],
+		};
+		return <Line data={data} />;
+	}
 }
 
-export default LineChart
+export default LineChart;
